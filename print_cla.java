dc17300942021096aacdc910cfcb3f61525d5de3
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-06-10 11:38:49 +0000

    print command line argument

---
+++ print_cla.java
@@ -0,0 +1,7 @@
+// input and print the elements from command line i.e terminal
+class demo1{
+    public static void main(String args[]){
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}

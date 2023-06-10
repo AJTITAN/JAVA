d8bb23e70f40e36a5cb20f0ccab2d6681bdc318b
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-06-10 17:05:25 +0000

    classes part 1

---
+++ classes1.java
@@ -0,0 +1,17 @@
+class box{
+    int a;
+    int b;
+    int c;
+}
+class demo2{
+    public static void main(String[] args) {
+        box obj = new box();
+        obj.a = 10;
+        obj.b = 11;
+        obj.c = 12;
+        System.out.println(obj.a);
+        System.out.println(obj.b);
+        System.out.println(obj.c);
+        
+    }
+}

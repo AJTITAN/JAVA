47b372f1fa5c2700c249f09dab582ea841620b88
Ashish Joshi <97600247+AJTITAN@users.noreply.github.com>
2023-06-10 10:46:13 +0000

    nyi shurwaat

--- 1.java
+++
@@ -1,6 +0,0 @@
-class demo{
-    public static void main(String arg[]){
-        System.out.println("The first command line argument is "+ arg[0]);
-        System.out.println("The first command line argument is "+ arg[1]);
-    }
-}
